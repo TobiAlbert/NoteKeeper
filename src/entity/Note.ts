0fd6f57535a81b122c62f7b9863e45185196c646
tobialbert <daadatobi@gmail.com>
2020-01-10 23:33:44 +0100

    Refactor: Create Relationship between User and Note.

--- src/entity/Note.ts
+++ src/entity/Note.ts
@@ -1,4 +1,5 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { User } from './User';
 
 @Entity()
 export class Note {
@@ -16,4 +17,7 @@ export class Note {
 
     @UpdateDateColumn()
     updatedAt: Date;
+
+    @ManyToOne(type => User, user => user.notes)
+    user: User;
 }
